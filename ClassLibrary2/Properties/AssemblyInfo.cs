b90267c934b8cffffe8d46816c4681bc4325377e
ivan-icko <ivanstepanovic6@gmail.com>
2022-06-26 17:49:24 +0200

    Promena naziva foldera

--- ClassLibrary2/Properties/AssemblyInfo.cs
+++ ClassLibrary2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary2")]
+[assembly: AssemblyTitle("ZooloskiVrt.Common.Domen.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary2")]
+[assembly: AssemblyProduct("ZooloskiVrt.Common.Domen.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
