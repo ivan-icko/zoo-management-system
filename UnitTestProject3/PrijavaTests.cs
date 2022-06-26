d8ebaabb8e3e63c5d588f415ac1408a9b6828091
ivan-icko <ivanstepanovic6@gmail.com>
2022-06-26 16:35:31 +0200

    Dokumentovan klasa Zaposleni

---
+++ UnitTestProject3/PrijavaTests.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using System;
+using ZooloskiVrt.Common.Domen;
+
+namespace UnitTestProject3
+{
+
+    public class PrijavaTests
+    {
+        public readonly Prijava p;
+        public PrijavaTests()
+        {
+            p = new Prijava();
+        }
+
+        [Fact]
+        public void SetPaketId_NegativeInt_Fact()
+        {
+          Assert.Throws<ArgumentOutOfRangeException>(() => p.SetIdPaketa(-1));
+        }
+
+        [Fact]
+        public void SetPaketId_Ok_Fact()
+        {
+            p.SetIdPaketa(1);
+            Assert.Equal(1,p.GetIdPaketa());
+        }
+
+        [Fact]
+        public void SetIdPosetioca_NegativeInt_Fact()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => p.SetIdPosetioca(-1));
+        }
+
+        [Fact]
+        public void SetIdPosetioca_Ok_Fact()
+        {
+            p.SetIdPaketa(1);
+            Assert.Equal(1, p.GetIdPaketa());
+        }
+
+        [Theory]
+        [InlineData("05/05/2005")]
+        public void SetDatumPrijave_Past_Theory(DateTime datum)
+        {
+            DateTime date = Convert.ToDateTime(datum);
+            Assert.Throws<ArgumentOutOfRangeException>(() => p.SetdDatumPrijave(datum));
+        }
+
+        [Theory]
+        [InlineData("10/10/2022")]
+        public void SetDatumPrijave_Ok_Theory(DateTime datum)
+        {
+            DateTime date = Convert.ToDateTime(datum);
+            p.SetdDatumPrijave(datum);
+            Assert.Equal(datum, p.GetDatumPrijave());
+        }
+
+
+
+        [Fact]
+        public void SetBrojOsoba_ZeroOfLess_Fact()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => p.SetBrojOsoba(-1));
+        }
+
+
+        [Fact]
+        public void SetBrojOsoba_Ok_Fact()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => p.SetBrojOsoba(33));
+        }
+
+
+    }
+}
--- ZooloskiVrt.Common.Domen/Prijava.cs
+++ ZooloskiVrt.Common.Domen/Prijava.cs
@@ -53,6 +53,88 @@ namespace ZooloskiVrt.Common.Domen
             return p;
         }
 
-        
+        /// <summary>
+        /// Metoda koja postavlja id paketa
+        /// </summary>
+        /// <param name="id">Id paketa</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void SetIdPaketa(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentOutOfRangeException("Id paketa ne sme biti manji od 0");
+            }
+            IdPaketa = id;
+        }
+        /// <summary>
+        /// Metoda koja vraca id paket
+        /// </summary>
+        /// <returns>Id paketa</returns>
+        public int GetIdPaketa()
+        {
+            return IdPaketa;
+        }
+        /// <summary>
+        /// Metoda koja postavlja id posetioca
+        /// </summary>
+        /// <param name="id">Id posetioca</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void SetIdPosetioca(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentOutOfRangeException("Id posetioca ne sme biti manji od 0");
+            }
+            IdPosetioca = id;
+        }
+
+        public int GetIdPosetioca()
+        {
+            return IdPosetioca;
+        }
+        /// <summary>
+        /// Metoda koja postalvlja datum prijave na paket
+        /// </summary>
+        /// <param name="datum">Datum prijave</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void SetdDatumPrijave(DateTime datum)
+        {
+
+            if (datum < DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException("Datum ne sme biti u proslosti");
+            }
+            DatumPrijave = datum;
+        }
+        /// <summary>
+        /// Funckija koja vraca datum paketa
+        /// </summary>
+        /// <returns>Datum do</returns>
+        public DateTime GetDatumPrijave()
+        {
+            return DatumPrijave;
+        }
+        /// <summary>
+        /// Metoda koja vraca broj osoba na paketu
+        /// </summary>
+        /// <param name="broj">Broj osoba</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void SetBrojOsoba(int broj)
+        {
+            if (broj < 1)
+            {
+                throw new ArgumentOutOfRangeException("Broj osoba ne sme biti manji od 1");
+            }
+            BrojOsoba = broj;
+        }
+        /// <summary>
+        /// Metoda koja vraca broj osoba na paketu
+        /// </summary>
+        /// <returns>Broj osoba</returns>
+        public int GetBrojOsoba()
+        {
+            return BrojOsoba;
+        }
+
     }
 }
